204bf7f361cb70a6c301d7cf86d2a15dbcd93ee6
Michael Orr <michael@cloudspace.com>
2014-04-02 19:01:33 +0000

    initial project generation using the Cloudspace Rails 4 Template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Justrails::Application.config.secret_key_base = 'c0b75ed2df82eb4e054cbd9a5a9a5f05b381dde06fc932e23f1dd6d1987896a7f401e893637af7c22497efbf1ac141105f156bc080732e194c20107db3c2b3b0'
